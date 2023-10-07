a7db8f35c75af5625725e1cea2e1e358ceecdae3
FourOrdination <forordination@tutanota.com>
2023-10-07 11:38:01 -0400

    Added piece jump

--- chess_scenes/pieces/piece_movement.gd
+++ chess_scenes/pieces/piece_movement.gd
@@ -5,6 +5,7 @@ var selected = false
 var board
 var row
 var col
+var piece_name
 #var which_selection: int
 func _ready():
 	board = get_parent().get_parent().board
@@ -12,22 +13,27 @@ func _ready():
 	print(col)
 	
 func _on_pressed():
-	if get_parent().get_parent().has_selected_piece_one == false:
-		get_parent().get_parent().source_row = row
-		get_parent().get_parent().source_col = col
-		await get_tree().create_timer(.5).timeout
-		get_parent().get_parent().has_selected_piece_one = true
-		print(get_parent().get_parent().source_row)
-		print(get_parent().get_parent().source_col)
-	elif get_parent().get_parent().has_selected_piece_one == true:
-		get_parent().get_parent().dest_row = row
-		get_parent().get_parent().dest_col = col
-		get_parent().get_parent().move_piece()
-		await get_tree().create_timer(.5).timeout
-		get_parent().get_parent().has_selected_piece_one = false
-		print(get_parent().get_parent().dest_row)
-		print(get_parent().get_parent().dest_col)
-	
+	get_parent().get_parent().player_clicked = true
+	#if get_parent().get_parent().has_selected_piece_one == false:
+	if get_parent().get_parent().player_clicked == true:
+		if get_parent().get_parent().has_selected_piece_one == false:
+			get_parent().get_parent().source_piece_name = piece_name
+			get_parent().get_parent().source_row = row
+			get_parent().get_parent().source_col = col
+			await get_tree().create_timer(.5).timeout
+			get_parent().get_parent().has_selected_piece_one = true
+			print(get_parent().get_parent().source_row)
+			print(get_parent().get_parent().source_col)
+		elif get_parent().get_parent().has_selected_piece_one == true:
+			get_parent().get_parent().dest_row = row
+			get_parent().get_parent().dest_col = col
+			get_parent().get_parent().move_piece()
+			await get_tree().create_timer(.5).timeout
+			get_parent().get_parent().has_selected_piece_one = false
+			print(get_parent().get_parent().dest_row)
+			print(get_parent().get_parent().dest_col)
+			#await get_tree().create_timer(.5).timeout
+			#get_parent().get_parent().player_clicked = false
 #func piece_selected(which_selection):
 #	match which_selection:
 #		1:
--- chess_scripts/chess_script.gd
+++ chess_scripts/chess_script.gd
@@ -40,6 +40,8 @@ func _ready():
 			row.append('E')
 		board.append(row)
 	# Place the initial piece on the board
+	row = randi() % (array_size - 1)
+	col = randi() % (array_size - 1)
 	randomize()
 	available_pieces.shuffle()
 	#get first piece and erase
@@ -48,7 +50,6 @@ func _ready():
 	available_pieces = available_pieces.filter(func(value): return value != null)
 	var scene_path = characterScenes[initial_piece]
 	var character_instance = load(scene_path)
-	print(scene_path)
 	# Create an instance of the scene
 	var character_node = character_instance.instantiate()
 	character_node.get_node("TouchScreenButton").row = row
@@ -57,8 +58,6 @@ func _ready():
 	
 	#place first piece random
 	print(initial_piece)
-	row = randi() % (array_size - 1)
-	col = randi() % (array_size - 1)
 	board[row][col] = initial_piece
 	character_node.position =  Vector2((col * cell_size_x) - 300, (row * cell_size_y) - 200)
 	# Place the remaining pieces on the board
@@ -94,7 +93,7 @@ func is_valid_move(row, col, piece):
 	# Check if the piece can capture any other piece
 	for i in range(array_size):
 		for j in range(array_size):
-			if board[i][j] != ' ':
+			if board[i][j] != 'E':
 				if piece == 'K' and (abs(row - i) <= 1 and abs(col - j) <= 1):
 					return true
 				elif piece == 'Q' and (row == i or col == j or abs(row - i) == abs(col - j)):
@@ -111,8 +110,9 @@ func is_valid_move(row, col, piece):
 	return false
 
 # Function to place the remaining pieces on the board
-# Function to place the remaining pieces on the board
+var has_assigned_row_col = false
 func place_remaining_pieces(initial_piece):
+	has_assigned_row_col = false
 	for i in range(0, num_pieces - 1):
 		var placed = false
 		while not placed:
@@ -169,13 +169,17 @@ func place_remaining_pieces(initial_piece):
 					print(scene_path)
 					# Create an instance of the scene
 					var character_node = character_instance.instantiate()
-
+					add_child(character_node)
 					# Add the character to your game scene (you may need to adjust the position)
 					# For example, if you have a parent node for characters:
 					character_node.position = Vector2((col * cell_size_x) - 300, (row * cell_size_y) - 200)
+					character_node.get_node("TouchScreenButton").piece_name = piece
+					#if has_assigned_row_col == false:
 					character_node.get_node("TouchScreenButton").row = row
 					character_node.get_node("TouchScreenButton").col = col
-					add_child(character_node)
+						#has_assigned_row_col = true
+					#if has_assigned_row_col == true:
+						#add_child(character_node)
 
 					break
 
@@ -202,18 +206,51 @@ var source_piece
 var dest_piece
 var has_selected_piece_one = false
 var has_selected_piece_two = false
+var source_piece_name
+func can_capture(sp, dr, dc, sr, sc):
+	var row_diff = abs(dr - sr)
+	var col_diff = abs(dc - sc)
+
+	if sp == 'K' and (row_diff <= 1 and col_diff <= 1):
+		return true
+	elif sp == 'Q' and (sr == dr or sc == dc or row_diff == col_diff):
+		return true
+	elif sp == 'R' and (sr == dr or sc == dc):
+		return true
+	elif sp == 'B' and row_diff == col_diff:
+		# Check if there are pieces in the way
+		var row_step = 1 if dr > sr else -1 if dr < sr else 0
+		var col_step = 1 if dc > sc else -1 if dc < sc else 0
+		var row = sr + row_step
+		var col = sc + col_step
+		while row != dr or col != dc:
+			if board[row][col] != 'E':
+				return false
+			row += row_step
+			col += col_step
+		return true
+	elif sp == 'N' and ((row_diff == 2 and col_diff == 1) or (row_diff == 1 and col_diff == 2)):
+		return true
+	elif sp == 'P' and ((dr == sr + 1 and col_diff == 1) or (dr == sr - 1 and col_diff == 1)):
+		return true
+
+	return false
+					
+var player_clicked = false
 func move_piece():
-	if has_selected_piece_one == true:
+	
+	if player_clicked == true:
+	#if has_selected_piece_one == true:
 		source_piece = board[source_row][source_col]
 		dest_piece = board[dest_row][dest_col]
 	
-		if is_valid_move(dest_row, dest_col, source_piece):
+		if can_capture(source_piece, dest_row, dest_col, source_row, source_col):
 			board[dest_row][dest_col] = source_piece
-			board[source_row][source_col] = " "
+			board[source_row][source_col] = 'E'
 		
-			return true
-		else:
-			if dest_piece != " ":
+			#return true
+		#else:
+			if dest_piece != 'E':
 				print("You have overriden " + dest_piece + " with " + source_piece + ".")
 				board[dest_row][dest_col] = source_piece
 				board[source_row][source_col] = 'E'
