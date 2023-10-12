37e78f7fb7ed5a35b06b7f05b6003a9abf5a8486
FourOrdination <forordination@tutanota.com>
2023-10-12 02:25:38 -0400

    Fixed Undo

---
+++ chess_scenes/undo.gd
@@ -0,0 +1,42 @@
+extends TouchScreenButton
+
+var first_piece
+var second_piece
+var i = 0
+
+func undo_move():
+	if global_row_column.previous_node.size() > 0 and global_row_column.hidden_node.size() > 0:
+		var pieceNodeName = global_row_column.previous_node[i]
+		var hiddenNodeName = global_row_column.hidden_node[i]
+
+		# Get references to the nodes
+		first_piece = get_node(pieceNodeName)
+		second_piece = get_node(hiddenNodeName)
+		
+		# Restore the position of the first piece to its previous position
+		first_piece.position = global_row_column.previous_node_position[i]
+
+		# Restore the row and column of the first piece
+		var button = first_piece.get_node("TouchScreenButton")
+		button.row = global_row_column.previous_node_row[i]
+		button.col = global_row_column.previous_node_col[i]
+
+		# Unhide the second piece
+		second_piece.show()
+		
+		# Print the data for debugging (optional)
+		print(global_row_column.previous_node)
+		print(global_row_column.hidden_node)
+		print(global_row_column.previous_node_position)
+		print(global_row_column.previous_node_row)
+		print(global_row_column.previous_node_col)
+		
+		# Remove the undone steps from the history lists
+		global_row_column.previous_node.erase(i)
+		global_row_column.hidden_node.erase(i)
+		global_row_column.previous_node_position.erase(i)
+		global_row_column.previous_node_row.erase(i)
+		global_row_column.previous_node_col.erase(i)
+
+		i += 1
+
--- chess_scripts/global_row_column.gd
+++ chess_scripts/global_row_column.gd
@@ -9,48 +9,55 @@ var first_col
 var second_row
 var second_col
 var player_click = false
-var previous_node = []
+var previous_node : Array
 var previous_node_position = []
 var previous_node_row = []
 var previous_node_col = []
-var hidden_node = []
+var hidden_node : Array
 var max_undo_steps = 3  # Maximum number of steps to undo
 
 # Called when the node enters the scene tree for the first time.
 func move_node():
-	get_node(str(first_piece_node)).position = get_node(str(second_piece_node)).position
-	get_node(str(first_piece_node) + "/TouchScreenButton").row = second_row
-	get_node(str(first_piece_node) + "/TouchScreenButton").col = second_col
-
 	previous_node.append(first_piece_node)
 	hidden_node.append(second_piece_node)
 	previous_node_position.append(get_node(str(first_piece_node)).position)
 	previous_node_row.append(first_row)
 	previous_node_col.append(first_col)
+	get_node(str(first_piece_node)).position = get_node(str(second_piece_node)).position
+	get_node(str(first_piece_node) + "/TouchScreenButton").row = second_row
+	get_node(str(first_piece_node) + "/TouchScreenButton").col = second_col
+	
+
+
 
 var action_history = []
 
+var i = 0
+var first_piece
+var second_piece
 func undo_move():
-	var num_steps_to_undo = min(previous_node.size(), max_undo_steps)
-
-	for i in range(num_steps_to_undo):
-		var first_piece = get_node(str(previous_node[i]))
-		var second_piece = get_node(str(hidden_node[i]))
+	print(previous_node)
+	if previous_node.size() > 0 and hidden_node.size() > 0:
+		print("undo")
+		get_parent().get_node("Pieces").undo_move()
+		first_piece = get_node("\"" + previous_node[0] + "\"")
+		second_piece = get_node("\"" + hidden_node[0] + "\"")
 
 		# Restore the position of the first piece to its previous position
-		first_piece.position = previous_node_position[i]
+		first_piece.position = previous_node_position[0]
 
 		# Restore the row and column of the first piece
-		first_piece.get_node("TouchScreenButton").row = previous_node_row[i]
-		first_piece.get_node("TouchScreenButton").col = previous_node_col[i]
+		first_piece.get_node("TouchScreenButton").row = previous_node_row[0]
+		first_piece.get_node("TouchScreenButton").col = previous_node_col[0]
 
 		# Unhide the second piece
 		second_piece.show()
 
-	# Remove the undone steps from the history lists
-	for i in range(num_steps_to_undo):
+		# Remove the undone steps from the history lists
 		previous_node.erase(0)
 		hidden_node.erase(0)
 		previous_node_position.erase(0)
 		previous_node_row.erase(0)
 		previous_node_col.erase(0)
+
+	i += 1
